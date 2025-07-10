e36e5c96f8e75accb43d1a776ed770b38afd4053
Llerraf Gnem <lleerraaff@gmail.com>
2025-07-10 14:34:54 -0500

    fish picture, configuration, color choosing

--- site/index.js
+++ site/index.js
@@ -1,6 +1,7 @@
 import { Chessground } from '@lichess-org/chessground';
 import { Chess } from 'chess.js'
 
+let playerColor = "white";
 let chess;
 let ground;
 const worker = new Worker(new URL("worker.js", import.meta.url), { type: "module" });
@@ -19,46 +20,90 @@ function updateDests() {
 }
 
 // TODO promotions!!!!!
-// chessground use
-async function externalMakeMove(moveFrom, moveTo, movePromo) {
+function moveUpdate(moveFrom, moveTo, movePromo) {
     worker.postMessage(["makeMove", moveFrom, moveTo, 0]);
     chess.move(moveFrom + moveTo + movePromo);
+    ground.set({ check: chess.inCheck() });
 }
 
-// code use
-function internalMakeMove(moveFrom, moveTo, movePromo) {
-    externalMakeMove(moveFrom, moveTo);
+function programmaticMove(moveFrom, moveTo, movePromo) {
     ground.move(moveFrom, moveTo);
-    ground.set({ turnColor: "white", movable: { color: "white" } });
+    ground.set({ turnColor: playerColor });
+    moveUpdate(moveFrom, moveTo);
     updateDests();
 }
+
+let troutImg = document.getElementById("trout-img");
+
+function troutNeutral() {
+    troutImg.src = "trout.png";
+    troutImg.alt = "trout neutral";
+}
+
+function troutThink() {
+    troutImg.src = "trout_think.png";
+    troutImg.alt = "trout thinking";
+}
+
+let maxDepthTimeElem = document.getElementById("max-depth-time");
+let bestMoveTime = maxDepthTimeElem.value;
+
+maxDepthTimeElem.addEventListener("change", () => {
+    bestMoveTime = maxDepthTimeElem.value;
+});
 
 const config = {
     fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
     movable: {
         free: false,
-        color: "white",
         events: {
             after: (orig, dest, metadata) => {
-                externalMakeMove(orig, dest, '');
-                worker.postMessage(["bestMove", 1000]);
+                moveUpdate(orig, dest, '');
+                troutThink();
+                worker.postMessage(["bestMove", bestMoveTime]);
             }
         }
     },
+    premovable: {
+        // too much extra code for little benefit (it's not even timed!)
+        enabled: false
+    }
 };
 
-function resetState() {
-    worker.postMessage(["reset"]);
-    chess = new Chess();
-    ground = Chessground(document.getElementById("board"), config);
-}
+chess = new Chess();
+ground = Chessground(document.getElementById("board"), config);
+let engineReady = false;
+let queueEngineStart = false;
+
+document.getElementById("white-overlay").addEventListener("click", () => {
+    playerColor = "white";
+    document.getElementById("color-chooser").style = "display: none";
+});
+document.getElementById("black-overlay").addEventListener("click", () => {
+    playerColor = "black";
+    document.getElementById("color-chooser").style = "display: none";
+    ground.set({ orientation: "black" });
+    if (engineReady) {
+        troutThink();
+        worker.postMessage(["bestMove", bestMoveTime]);
+    } else {
+        queueEngineStart = true;
+    }
+});
+
+updateDests();
 
 worker.onmessage = (e) => {
-    if (e.data == "init") {
-        resetState();
-        updateDests();
+    if (e.data === "init") {
+        worker.postMessage(["reset"]);
+        engineReady = true;
+        if (queueEngineStart) {
+            troutThink();
+            worker.postMessage(["bestMove", bestMoveTime]);
+        }
     } else {
         const [f, t, p] = e.data;
-        internalMakeMove(f, t, p);
+        programmaticMove(f, t, p);
+        troutNeutral();
     }
 };
